a47548882ffc653adf11b38ac78b8d9f37754ba8
Shaked Falah <shaked341@gmail.com>
2026-07-09 00:04:20 +0300

    added animator

---
+++ MonogameProject/MyEngine/Components/Animator.cs
@@ -0,0 +1,38 @@
+﻿using MonogameProject.MyEngine.Attributes;
+using MonogameProject.MyEngine.Interfaces;
+using MonogameProject.MyEngine.Rendering;
+
+namespace MonogameProject.MyEngine.Components
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    internal class Animator : Component, IUpdateable, IStartable
+    {
+        private AnimationClip _clip;
+        private SpriteRenderer _spriteRenderer;
+        private float _timeTillNextFrame = 1f;
+        private float _timeBetweenFrames = 1f;
+
+
+        public void Start()
+        {
+            _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        }
+
+        public void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            _timeBetweenFrames = 1f / _clip.frameRate;
+            _timeTillNextFrame -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (_timeTillNextFrame < 0f)
+            {
+                _timeTillNextFrame = _timeBetweenFrames;
+                _spriteRenderer.sprite = _clip.next();
+            }
+        }
+
+        public void SetClip(AnimationClip clip)
+        {
+            _clip = clip;
+        }
+    }
+}
