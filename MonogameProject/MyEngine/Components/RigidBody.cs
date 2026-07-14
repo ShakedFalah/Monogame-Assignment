0ea0aa23e07aa8d2c6516753c0ea6e9ffc96a747
Shaked Falah <shaked341@gmail.com>
2026-07-14 21:05:46 +0300

    added rigid body class

---
+++ MonogameProject/MyEngine/Components/RigidBody.cs
@@ -0,0 +1,10 @@
+﻿using MonogameProject.MyEngine.Attributes;
+using MonogameProject.MyEngine.Components;
+
+namespace MonogameProject.MyEngine.Components
+{
+    [RequireComponent(typeof(Collider))]
+    internal class RigidBody : Component
+    {
+    }
+}
